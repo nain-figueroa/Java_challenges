a98b97c828d58ab0a8b2bd3c5981b66e373ed6ed
nain-figueroa <nainzavala7@gmail.com>
2025-01-18 11:32:59 -0700

    Tercer programa 2.0

--- palindromo/Is_palindromo.java
+++ palindromo/Is_palindromo.java
@@ -6,19 +6,26 @@ public class Is_palindromo {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String word = "", reversed_word = "";
+        String word = "", reversed_word = "", word_no_spaces = "";
         System.out.println("===Palindromos===");
         System.out.print("Palabra: ");
         word = input.nextLine();
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed_word += word.charAt(i);
+        for (int i = 0; i <= word.length() - 1; i++) {
+            if (word.charAt(i) != ' ') {
+                word_no_spaces += word.charAt(i);
+            }
+        }
+        for (int i = word_no_spaces.length() - 1; i >= 0; i--) {
+            if (word_no_spaces.charAt(i) != ' ') {
+                reversed_word += word_no_spaces.charAt(i);
+            }
         }
 
-        word = word.toLowerCase();
+        word_no_spaces = word_no_spaces.toLowerCase();
         reversed_word = reversed_word.toLowerCase();
 
-        if (!word.equals(reversed_word)) {
+        if (!word_no_spaces.equals(reversed_word)) {
             System.out.println(word + " NO es un palindromo");
         } else {
             System.out.println(word + " Es un palindromo");
